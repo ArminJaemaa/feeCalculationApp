d9167771b804fc13a6b8ee041ce3b7f3fe55d09f
ArminJaemaa <ajaemaa@tlu.ee>
2026-03-23 17:00:20 +0200

    feat: add delivery fee controller GET endpoint

---
+++ feeCalculator/src/main/java/ee/armin_jaemaa/feeCalculator/controller/DeliveryFeeController.java
@@ -0,0 +1,27 @@
+package ee.armin_jaemaa.feeCalculator.controller;
+
+import ee.armin_jaemaa.feeCalculator.model.City;
+import ee.armin_jaemaa.feeCalculator.model.VehicleType;
+import ee.armin_jaemaa.feeCalculator.service.DeliveryFeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class DeliveryFeeController {
+    private final DeliveryFeeService deliveryFeeService;
+
+    @GetMapping("/delivery-fee")
+    public ResponseEntity<Double> getDeliveryFee(
+            @RequestParam  City city,
+            @RequestParam VehicleType vehicle){
+
+        double totalFee = deliveryFeeService.calculateTotalFee(city, vehicle);
+        return ResponseEntity.ok(totalFee);
+    }
+}
