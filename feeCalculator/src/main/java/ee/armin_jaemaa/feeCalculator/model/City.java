23f8c40fc72ec14db89a278ed433f6753a4eb63e
ArminJaemaa <ajaemaa@tlu.ee>
2026-03-23 16:59:44 +0200

    fix: connect city name to station name and correct temperature logic

--- feeCalculator/src/main/java/ee/armin_jaemaa/feeCalculator/model/City.java
+++ feeCalculator/src/main/java/ee/armin_jaemaa/feeCalculator/model/City.java
@@ -1,5 +1,16 @@
 package ee.armin_jaemaa.feeCalculator.model;
 
+import lombok.Getter;
+
+@Getter
 public enum City {
-    TALLINN, TARTU, PÄRNU
+    TALLINN("Tallinn-Harku"),
+    TARTU("Tartu-Tõravere"),
+    PÄRNU("Pärnu");
+
+    private final String stationName;
+
+    City(String stationName) {
+        this.stationName = stationName;
+    }
 }
