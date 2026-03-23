70fcd86c0654cc942ba26d276fb7d7dbc86906fb
ArminJaemaa <ajaemaa@tlu.ee>
2026-03-23 17:54:25 +0200

    feat: add swagger api documentation

--- feeCalculator/src/main/java/ee/armin_jaemaa/feeCalculator/controller/DeliveryFeeController.java
+++ feeCalculator/src/main/java/ee/armin_jaemaa/feeCalculator/controller/DeliveryFeeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-public class DeliveryFeeController {
+public class DeliveryFeeController implements DeliveryFeeApi {
     private final DeliveryFeeService deliveryFeeService;
 
     @GetMapping("/delivery-fee")
