cec0b53b9a44a05bfbed2675152383124f3ffb9c
ArminJaemaa <ajaemaa@tlu.ee>
2026-03-23 16:26:16 +0200

    refactor(VehicleForbiddenException): change vehicle forbidden message.

--- feeCalculator/src/main/java/ee/armin_jaemaa/feeCalculator/exception/VehicleForbiddenException.java
+++ feeCalculator/src/main/java/ee/armin_jaemaa/feeCalculator/exception/VehicleForbiddenException.java
@@ -1,7 +1,9 @@
 package ee.armin_jaemaa.feeCalculator.exception;
 
+import ee.armin_jaemaa.feeCalculator.model.VehicleType;
+
 public class VehicleForbiddenException extends RuntimeException {
-    public VehicleForbiddenException(String message) {
-        super(message);
+    public VehicleForbiddenException() {
+        super("Usage of selected vehicle type is forbidden");
     }
 }
