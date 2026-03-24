3373a3923da446caf0c34ef525796c883529d532
ArminJaemaa <ajaemaa@tlu.ee>
2026-03-24 19:20:41 +0200

    test: fix tests with improved fee history data

--- feeCalculator/src/test/java/ee/armin_jaemaa/feeCalculator/service/DeliveryFeeServiceTest.java
+++ feeCalculator/src/test/java/ee/armin_jaemaa/feeCalculator/service/DeliveryFeeServiceTest.java
@@ -16,11 +16,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,7 +38,7 @@ public class DeliveryFeeServiceTest {
     @ParameterizedTest
     @EnumSource(value = VehicleType.class, names = {"BIKE", "SCOOTER"})
     public void windSpeedTooHighTest(VehicleType vehicle) {
-        BaseFee mockBaseFee = new BaseFee(1L, City.TALLINN, vehicle, 3.0);
+        BaseFee mockBaseFee = new BaseFee(1L, City.TALLINN, vehicle, 3.0, LocalDateTime.now());
         WeatherData mockWeather = WeatherData.builder()
                 .stationName("Tallinn-Harku")
                 .windSpeed(100.0)
@@ -45,20 +46,20 @@ public class DeliveryFeeServiceTest {
                 .temperature(15.0)
                 .build();
 
-        when(baseFeeRepository.findByCityAndVehicleType(any(), any()))
+        when(baseFeeRepository.findFirstByCityAndVehicleTypeAndTimestampLessThanEqualOrderByTimestampDesc(eq(City.TALLINN), eq(vehicle), any(LocalDateTime.class)))
                 .thenReturn(Optional.of(mockBaseFee));
-        when(weatherDataRepository.findFirstByStationNameOrderByTimestampDesc("Tallinn-Harku"))
+        when(weatherDataRepository.findFirstByStationNameAndTimestampLessThanEqualOrderByTimestampDesc(eq("Tallinn-Harku"),any(LocalDateTime.class)))
                 .thenReturn(Optional.of(mockWeather));
 
         VehicleForbiddenException exception = assertThrows(VehicleForbiddenException.class, () -> {
-            deliveryFeeService.calculateTotalFee(City.TALLINN, vehicle);
+            deliveryFeeService.calculateTotalFee(City.TALLINN, vehicle, LocalDateTime.now());
         });
         assertEquals("Usage of selected vehicle type is forbidden", exception.getMessage());
     }
 
     @Test
     public void calculateTotalFeeTest() {
-        BaseFee mockBaseFee =  new BaseFee(1L, City.TARTU, VehicleType.CAR, 3.5);
+        BaseFee mockBaseFee =  new BaseFee(1L, City.TARTU, VehicleType.CAR, 3.5,  LocalDateTime.now());
         WeatherData mockWeather = WeatherData.builder()
                 .stationName("Tartu-Tõravere")
                 .windSpeed(100.0)
@@ -66,12 +67,12 @@ public class DeliveryFeeServiceTest {
                 .temperature(15.0)
                 .build();
 
-        when(baseFeeRepository.findByCityAndVehicleType(City.TARTU, VehicleType.CAR))
+        when(baseFeeRepository.findFirstByCityAndVehicleTypeAndTimestampLessThanEqualOrderByTimestampDesc(eq(City.TARTU), eq(VehicleType.CAR), any(LocalDateTime.class)))
                 .thenReturn(Optional.of(mockBaseFee));
-        when(weatherDataRepository.findFirstByStationNameOrderByTimestampDesc("Tartu-Tõravere"))
+        when(weatherDataRepository.findFirstByStationNameAndTimestampLessThanEqualOrderByTimestampDesc(eq("Tartu-Tõravere"), any(LocalDateTime.class)))
                 .thenReturn(Optional.of(mockWeather));
 
-        assertEquals(3.5, deliveryFeeService.calculateTotalFee(City.TARTU, VehicleType.CAR));
+        assertEquals(3.5, deliveryFeeService.calculateTotalFee(City.TARTU, VehicleType.CAR,  LocalDateTime.now()));
     }
 
     @ParameterizedTest
@@ -82,7 +83,7 @@ public class DeliveryFeeServiceTest {
             "BIKE, Clear, -5, 3, false"
     })
     public void calculateVehicleExtraFeeTest(VehicleType vehicle, String phenomenon, double temperature, double fee, boolean expectedFail) {
-        BaseFee mockBaseFee = new BaseFee(1L, City.TALLINN, vehicle, fee);
+        BaseFee mockBaseFee = new BaseFee(1L, City.TALLINN, vehicle, fee, LocalDateTime.now());
 
         WeatherData mockWeather = WeatherData.builder()
                 .stationName("Tallinn-Harku")
@@ -91,19 +92,20 @@ public class DeliveryFeeServiceTest {
                 .temperature(temperature)
                 .build();
 
-        when(baseFeeRepository.findByCityAndVehicleType(City.TALLINN, vehicle))
+        when(baseFeeRepository.findFirstByCityAndVehicleTypeAndTimestampLessThanEqualOrderByTimestampDesc(eq(City.TALLINN), eq(vehicle), any(LocalDateTime.class)))
                 .thenReturn(Optional.of(mockBaseFee));
-        when(weatherDataRepository.findFirstByStationNameOrderByTimestampDesc("Tallinn-Harku"))
+        when(weatherDataRepository.findFirstByStationNameAndTimestampLessThanEqualOrderByTimestampDesc(eq("Tallinn-Harku"), any(LocalDateTime.class)))
                 .thenReturn(Optional.of(mockWeather));
 
         if(expectedFail) {
             assertThrows(VehicleForbiddenException.class, () -> {
-                deliveryFeeService.calculateTotalFee(City.TALLINN, vehicle);
+                deliveryFeeService.calculateTotalFee(City.TALLINN, vehicle,  LocalDateTime.now());
             });
             //System.out.println("expectedFail");
         }else{
-            double result = deliveryFeeService.calculateTotalFee(City.TALLINN, vehicle);
+            double result = deliveryFeeService.calculateTotalFee(City.TALLINN, vehicle,   LocalDateTime.now());
             //System.out.println(result);
+            assertTrue(result >= fee, "Total fee has to be atleast the same as base fee");
         }
     }
 }
